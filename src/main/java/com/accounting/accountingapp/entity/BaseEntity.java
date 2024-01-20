26c63af81bfceae92d9ba2b39e22360e3d3f6c05
nihano <nioztekin@gmail.com>
2024-01-20 18:57:41 +0300

    BaseEntity

---
+++ src/main/java/com/accounting/accountingapp/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.accounting.accountingapp.entity;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+
+@NoArgsConstructor
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime insertDateTime;
+    @Column(nullable = false, updatable = false)
+    private Long insertUserId;
+    @Column(nullable = false)
+    private LocalDateTime lastUpdateDateTime;
+    private boolean isDeleted;
+
+
+}
