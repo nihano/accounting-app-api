8c0394fa02fd32551b39744e53688ac32160d7d4
nihano <nioztekin@gmail.com>
2024-01-21 16:48:11 +0300

    Address entity

---
+++ src/main/java/com/accounting/accountingapp/entity/Address.java
@@ -0,0 +1,23 @@
+package com.accounting.accountingapp.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "addresses")
+public class Address extends BaseEntity{
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String country;
+    private String zipCode;
+
+}
