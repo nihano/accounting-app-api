2a9ea4c19df9c8b5b5934768fc7b51bf2da44e7e
nihano <nioztekin@gmail.com>
2024-01-22 01:06:07 +0300

    BaseEntity

--- src/main/java/com/accounting/accountingapp/entity/BaseEntity.java
+++ src/main/java/com/accounting/accountingapp/entity/BaseEntity.java
@@ -1,25 +1,36 @@
 package com.accounting.accountingapp.entity;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 
-
 @MappedSuperclass
 @NoArgsConstructor
+@Getter
+@Setter
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false, updatable = false)
-    private LocalDateTime insertDateTime;
+    public LocalDateTime insertDateTime=LocalDateTime.now();
+
     @Column(nullable = false, updatable = false)
-    private Long insertUserId;
+    public Long insertUserId=1L;
+
     @Column(nullable = false)
-    private LocalDateTime lastUpdateDateTime;
-    private boolean isDeleted;
+    public LocalDateTime lastUpdateDateTime=LocalDateTime.now();
+
+    @Column(nullable = false)
+    public Long lastUpdateUserId=1l;
+
+    private boolean isDeleted=false;
+
 
 
 }
