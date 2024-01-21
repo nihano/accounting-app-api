c22c6deb7db31a72ab85b874ab09c7965029c0ed
nihano <nioztekin@gmail.com>
2024-01-22 01:05:30 +0300

    UserDto

--- src/main/java/com/accounting/accountingapp/entity/User.java
+++ src/main/java/com/accounting/accountingapp/entity/User.java
@@ -9,8 +9,8 @@ public class User extends BaseEntity {
     @Column(unique = true)
     private String username;
     private String password;
-    private String firstName;
-    private String lastName;
+    private String firstname;
+    private String lastname;
     private String phone;
     private boolean enabled;
     /// many-to-one / will be seen under "role_id" column on the "users" table
