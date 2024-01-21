6a17104a37955f3c89d7de3b987f6f186bc12c37
nihano <nioztekin@gmail.com>
2024-01-21 16:51:04 +0300

    modelMapper bean

--- src/main/java/com/accounting/accountingapp/AccountingAppApplication.java
+++ src/main/java/com/accounting/accountingapp/AccountingAppApplication.java
@@ -1,7 +1,10 @@
 package com.accounting.accountingapp;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 
 @SpringBootApplication
 public class AccountingAppApplication {
@@ -10,4 +13,8 @@ public class AccountingAppApplication {
         SpringApplication.run(AccountingAppApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
