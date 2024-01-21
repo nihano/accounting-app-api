63a12e6ff1e0c7a15b3acd9f6a56690fbf300c7c
nihano <nioztekin@gmail.com>
2024-01-22 01:10:41 +0300

    adding createOrUpdateAddress method for existing Address entities

---
+++ src/main/java/com/accounting/accountingapp/service/impl/AddressServiceImpl.java
@@ -0,0 +1,49 @@
+package com.accounting.accountingapp.service.impl;
+
+import com.accounting.accountingapp.dto.AddressDto;
+import com.accounting.accountingapp.entity.Address;
+import com.accounting.accountingapp.mapper.MapperUtil;
+import com.accounting.accountingapp.repository.AddressRepository;
+import com.accounting.accountingapp.service.AddressService;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+
+    private final MapperUtil mapperUtil;
+    private final AddressRepository addressRepository;
+
+    public AddressServiceImpl(MapperUtil mapperUtil, AddressRepository addressRepository) {
+        this.mapperUtil = mapperUtil;
+        this.addressRepository = addressRepository;
+    }
+
+    @Override
+    public void save(AddressDto addressDto) {
+        Address address = mapperUtil.convert(addressDto, new Address());
+        addressRepository.save(address);
+    }
+
+    @Override
+    public Address createOrUpdateAddress(AddressDto addressDto) {
+        Address address = mapperUtil.convert(addressDto, new Address());
+
+        Optional<Address> existingAddress = addressRepository.findByAddressDetails(
+                address.getAddressLine1(),
+                address.getAddressLine2(),
+                address.getCity(),
+                address.getState(),
+                address.getZipCode()
+        );
+
+        if (existingAddress.isPresent()) {
+            address = existingAddress.get();
+        } else {
+            addressRepository.save(address);
+        }
+
+        return address;
+    }
+}
