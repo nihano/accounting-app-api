cf438caba5e285868592797b7d6747a36d9aa287
nihano <nioztekin@gmail.com>
2024-01-22 01:05:39 +0300

    RoleDto

---
+++ src/main/java/com/accounting/accountingapp/dto/RoleDto.java
@@ -0,0 +1,9 @@
+package com.accounting.accountingapp.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class RoleDto {
+
+    private Long id;
+    private String description;
+}
