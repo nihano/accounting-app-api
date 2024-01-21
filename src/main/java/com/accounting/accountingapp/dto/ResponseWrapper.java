27149630c622220e18c8d8dd24a2353fc439a55f
nihano <nioztekin@gmail.com>
2024-01-21 16:49:52 +0300

    ResponseWrapper

---
+++ src/main/java/com/accounting/accountingapp/dto/ResponseWrapper.java
@@ -0,0 +1,27 @@
+package com.accounting.accountingapp.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+public class ResponseWrapper {
+    private boolean success;
+    private String message;
+    private Integer code;
+    private Object data;
+
+
+    public ResponseWrapper(String message, Object data){
+        this.message=message;
+        this.data=data;
+        this.code= HttpStatus.OK.value();
+        this.success=true;
+    }
+
+    //for delete
+    public ResponseWrapper(String message){
+        this.message=message;
+        this.code= HttpStatus.OK.value();
+        this.success=true;
+    }
+}
