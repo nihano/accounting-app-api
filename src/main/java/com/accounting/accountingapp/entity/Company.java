2a5a9d552e4dda61a6e39dc3b8b33399275d13ff
nihano <nioztekin@gmail.com>
2024-01-21 16:46:57 +0300

    Company entity

---
+++ src/main/java/com/accounting/accountingapp/entity/Company.java
@@ -0,0 +1,26 @@
+package com.accounting.accountingapp.entity;
+
+import com.accounting.accountingapp.enums.CompanyStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "companies")
+public class Company extends BaseEntity {
+    @Column(unique = true)
+    private String title;
+    private String phone;
+    private String website;
+
+    @Enumerated(EnumType.STRING)
+    private CompanyStatus companyStatus;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Address address;
+}
