581cee96717980561d76972c36a9c84bdc0d5129
nihano <nioztekin@gmail.com>
2024-01-23 16:00:19 +0300

    @JsonIgnoreProperties

--- src/main/java/com/accounting/accountingapp/dto/RoleDto.java
+++ src/main/java/com/accounting/accountingapp/dto/RoleDto.java
@@ -1,7 +1,18 @@
 package com.accounting.accountingapp.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RoleDto {
 
     private Long id;
