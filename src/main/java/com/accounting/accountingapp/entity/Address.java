8947d68e8155ccd49d15114755a4d930637bd619
nihano <nioztekin@gmail.com>
2024-01-23 15:58:06 +0300

    @ToString for logs

--- src/main/java/com/accounting/accountingapp/entity/Address.java
+++ src/main/java/com/accounting/accountingapp/entity/Address.java
@@ -3,6 +3,7 @@ package com.accounting.accountingapp.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -11,6 +12,7 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @Getter
 @Setter
+@ToString
 @Table(name = "addresses")
 public class Address extends BaseEntity{
     private String addressLine1;
