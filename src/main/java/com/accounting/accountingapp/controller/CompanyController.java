db9b55136e76f0d8f7c5b9913e7c06ef03516146
nihano <nioztekin@gmail.com>
2024-01-23 16:02:31 +0300

    adding update, activate, deactivate

--- src/main/java/com/accounting/accountingapp/controller/CompanyController.java
+++ src/main/java/com/accounting/accountingapp/controller/CompanyController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/company")
 public class CompanyController {
@@ -18,19 +20,48 @@ public class CompanyController {
     }
 
 
-    @GetMapping("/create")
-    public ResponseEntity<ResponseWrapper> getCompanies(){
+    @GetMapping("/list")
+    public ResponseEntity<ResponseWrapper> getCompanies() {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(new ResponseWrapper("Companies successfully retrieved", companyService.listCompanies()));
     }
 
-
-    @PostMapping("/create")
-    public ResponseEntity<ResponseWrapper> createCompany(@RequestBody CompanyDto companyDto){
+    @GetMapping("/create")
+    public ResponseEntity<ResponseWrapper> getCompany(@RequestBody CompanyDto companyDto) {
 
         companyService.save(companyDto);
         return ResponseEntity.ok(new ResponseWrapper("Company is successfully created", companyDto));
     }
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseWrapper> createCompany(@RequestBody CompanyDto companyDto) {
+
+        companyService.save(companyDto);
+        return ResponseEntity.ok(new ResponseWrapper("Company is successfully created", companyDto));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<ResponseWrapper> updateCompany(@PathVariable("id") Long id, @RequestBody CompanyDto companyDto) {
+        companyService.update(companyDto, id);
+        return ResponseEntity.ok(new ResponseWrapper("Company is successfully updated", companyDto));
+    }
+
+    @GetMapping("/activate/{id}")
+    public ResponseEntity<ResponseWrapper> activateCompany(@PathVariable("id") Long id) {
+        companyService.activateCompany(id);
+        return ResponseEntity.
+                status(HttpStatus.OK)
+                .body(new ResponseWrapper("Company status is successfully updated as 'Active' ", HttpStatus.OK));
+    }
+
+    @GetMapping("/deactivate/{id}")
+    public ResponseEntity<ResponseWrapper> deactivateCompany(@PathVariable("id") Long id) {
+        companyService.deactivateCompany(id);
+        return ResponseEntity.
+                status(HttpStatus.OK)
+                .body(new ResponseWrapper("Company status is successfully updated as 'Passive'", HttpStatus.OK));
+    }
+
 
 }
