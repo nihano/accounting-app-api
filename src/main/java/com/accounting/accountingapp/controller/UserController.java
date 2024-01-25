2c581f74daf46a010c784b118a0ca75fef18f9ff
nihano <nioztekin@gmail.com>
2024-01-25 23:01:57 +0300

    adding GET and POST methods for /create

--- src/main/java/com/accounting/accountingapp/controller/UserController.java
+++ src/main/java/com/accounting/accountingapp/controller/UserController.java
@@ -1,12 +1,11 @@
 package com.accounting.accountingapp.controller;
 
 import com.accounting.accountingapp.dto.ResponseWrapper;
+import com.accounting.accountingapp.dto.UserDto;
 import com.accounting.accountingapp.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -24,5 +23,17 @@ public class UserController {
         return ResponseEntity.ok()
                 .body(new ResponseWrapper("Users are successfully retrieved", userService.listUsers()));
 
+    }
+
+    @GetMapping("/create")
+    public ResponseEntity<ResponseWrapper> getCreateUserForm(){
+        return ResponseEntity.ok(new ResponseWrapper("User create page is successfully retrieved",new UserDto()));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseWrapper> createUser(@RequestBody UserDto userDto){
+        userService.save(userDto);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ResponseWrapper("User is successfully created", userDto));
     }
 }
