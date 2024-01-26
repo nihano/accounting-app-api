b2e2f6d8936bcc56ec9c6b76c2beb96d766a5974
nihano <nioztekin@gmail.com>
2024-01-26 12:20:05 +0300

    modified query

--- src/main/java/com/accounting/accountingapp/repository/CompanyRepository.java
+++ src/main/java/com/accounting/accountingapp/repository/CompanyRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
-    @Query("SELECT c FROM Company c WHERE c.id<>1 ORDER BY c.companyStatus")
-    List<Company> listCompaniesByStatus();
+    @Query("SELECT c FROM Company c WHERE c.id<>1 AND c.isDeleted=false ORDER BY c.companyStatus")
+    List<Company> listCompaniesByStatusAndIsDeletedFalse();
+
+    Company findByTitle(String title);
 }
