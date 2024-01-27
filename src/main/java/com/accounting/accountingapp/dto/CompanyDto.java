bc6067b2e25774a19d297d62761a99a870c28d38
nihano <nioztekin@gmail.com>
2024-01-27 22:09:34 +0300

    adding toString

--- src/main/java/com/accounting/accountingapp/dto/CompanyDto.java
+++ src/main/java/com/accounting/accountingapp/dto/CompanyDto.java
@@ -4,19 +4,18 @@ import com.accounting.accountingapp.enums.CompanyStatus;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CompanyDto {
 
+    @JsonIgnore
     private Long id;
     private String title;
     private String phone;
