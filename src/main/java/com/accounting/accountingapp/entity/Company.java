3e033f99210e82901a2fca5087fb3180439126ea
nihano <nioztekin@gmail.com>
2024-01-27 22:08:29 +0300

    Company

--- src/main/java/com/accounting/accountingapp/entity/Company.java
+++ src/main/java/com/accounting/accountingapp/entity/Company.java
@@ -1,28 +1,28 @@
-package com.accounting.accountingapp.entity;
+    package com.accounting.accountingapp.entity;
 
-import com.accounting.accountingapp.enums.CompanyStatus;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+    import com.accounting.accountingapp.enums.CompanyStatus;
+    import lombok.Getter;
+    import lombok.NoArgsConstructor;
+    import lombok.Setter;
+    import lombok.ToString;
 
-import javax.persistence.*;
+    import javax.persistence.*;
 
-@Entity
-@NoArgsConstructor
-@Getter
-@Setter
-@ToString
-@Table(name = "companies")
-public class Company extends BaseEntity {
-    @Column(unique = true)
-    private String title;
-    private String phone;
-    private String website;
+    @Entity
+    @NoArgsConstructor
+    @Getter
+    @Setter
+    @ToString
+    @Table(name = "companies")
+    public class Company extends BaseEntity {
+        @Column(unique = true)
+        private String title;
+        private String phone;
+        private String website;
 
-    @Enumerated(EnumType.STRING)
-    private CompanyStatus companyStatus;
+        @Enumerated(EnumType.STRING)
+        private CompanyStatus companyStatus;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    private Address address;
-}
+        @OneToOne(fetch = FetchType.LAZY)
+        private Address address;
+    }
