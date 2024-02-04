fabde05a9cf6157fb58d937e0719176fe48e092a
nihano <nioztekin@gmail.com>
2024-02-04 22:02:04 +0300

    adding data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -35,3 +35,39 @@ values ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
        ('2022-09-15 00:00:00', 1, false, '2022-09-15 00:00:00', 1,
         'Red Tech','+1 (215) 846-2642', 'https://www.redtech.com', 4, 'PASSIVE');
 
+
+insert into users(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id,
+                  username, password, firstname, lastname, phone, role_id, company_id, enabled)
+values
+-- COMPANY-1 / CYDEO / ROOT USER
+('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
+ 'root@cydeo.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
+ 'Robert', 'Martin', '+1 (852) 564-5874', 1, 1, true),
+-- COMPANY-2 / Green Tech / ADMIN-1
+('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
+ 'admin@greentech.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
+ 'Mary', 'Grant', '+1 (234) 345-4362', 2, 2, true),
+-- COMPANY-2 / Green Tech / ADMIN-2
+('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
+ 'admin2@greentech.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
+ 'Garrison', 'Short', '+1 (234) 356-7865', 2, 2, true),
+-- COMPANY-2 / Green Tech / MANAGER
+('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
+ 'manager@greentech.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
+ 'Robert', 'Noah', '+1 (234) 564-5874', 3, 2, true),
+-- COMPANY-2 / Green Tech / EMPLOYEE
+('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
+ 'employee@greentech.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
+ 'Mike', 'Times', '+1 (234) 741-8569', 4, 2, true),
+-- COMPANY-3 / Blue Tech / ADMIN
+('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
+ 'admin@bluetech.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
+ 'Chris', 'Brown', '+1 (356) 258-3544', 2, 3, true),
+-- COMPANY-3 / Blue Tech / MANAGER
+('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
+ 'manager@bluetech.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
+ 'Tom', 'Hanks', '+1 (356) 258-3544', 3, 3, true),
+-- COMPANY-4 / Red Tech / ADMIN
+('2022-09-09 00:00:00', 1, false, '2022-09-09 00:00:00', 1,
+ 'admin@redtech.com', '$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK',
+ 'John', 'Doe', '+1 (659) 756-1265', 2, 4, true);
